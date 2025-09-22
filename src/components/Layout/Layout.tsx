c058f52686c8f93a60eb669ce532a8ca31348bdc
Halina Tulchii <halendeihalyna@gmail.com>
2025-09-22 16:22:54 +0300

    updated Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,14 @@
-import { useLocation } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const location = useLocation();
-  const pathname = location.pathname;
 
-  const isLightText = ["/loads", "/contact", "/policy"].includes(pathname);
-  const headerTextColor = isLightText ? "light" : "dark";
+
+
+
   return (
     <div>
-      <Header themeColor={headerTextColor} />
+      <Header/>
       <main>{children}</main>
       <Footer />
     </div>
