4f9574ad7da9c0f44957955565d22c31bbcf95aa
Halina Tulchii <halendeihalyna@gmail.com>
2025-10-10 12:15:22 +0300

    added .env to gitignore

--- src/sections/Home/Hero/Hero.tsx
+++ src/sections/Home/Hero/Hero.tsx
@@ -1,90 +1,90 @@
 import { useTranslation } from "react-i18next";
 import { NavLink } from "react-router-dom";
-import { useEffect, useRef } from "react";
+// import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import './hero.scss'
-import { HERO_IMG } from "./Hero.Constants";
+// import { HERO_IMG } from "./Hero.Constants";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
     const { t } = useTranslation("homeHero");
-    const imgRef = useRef<HTMLImageElement | null>(null);
-  const sectionRef = useRef<HTMLDivElement | null>(null);
+    // const imgRef = useRef<HTMLImageElement | null>(null);
+  // const sectionRef = useRef<HTMLDivElement | null>(null);
   
 
-  useEffect(() => {
-    if (!sectionRef.current || !imgRef.current) return;
+  // useEffect(() => {
+  //   if (!sectionRef.current || !imgRef.current) return;
 
-    const mm = gsap.matchMedia();
+  //   const mm = gsap.matchMedia();
 
-    mm.add("(min-width: 1551px)", () => {
-      // 📌 Десктоп
-      gsap.set(imgRef.current, {
-        width: "172px",
-        height: "65px",
-        borderRadius: "20px",
-        position: "absolute",
-        top: "230px",
-        left: "695px",
-        xPercent: 0,
-        yPercent: 0,
-      });
+  //   mm.add("(min-width: 1551px)", () => {
+  //     // 📌 Десктоп
+  //     gsap.set(imgRef.current, {
+  //       width: "172px",
+  //       height: "65px",
+  //       borderRadius: "20px",
+  //       position: "absolute",
+  //       top: "230px",
+  //       left: "695px",
+  //       xPercent: 0,
+  //       yPercent: 0,
+  //     });
 
-      gsap.to(imgRef.current, {
-        width: "100vw",
-        height: "100vh",
-        borderRadius: 0,
-        top: 0,
-        left: "0%",
-        xPercent: 0,
-        yPercent: 50,
-        ease: "expo.out",
-        scrollTrigger: {
-          trigger: sectionRef.current,
-          start: "top top",
-          end: "top+=1000vh",
-          scrub: true,
-          pin: false,
-        },
-      });
-    });
+  //     gsap.to(imgRef.current, {
+  //       width: "100vw",
+  //       height: "100vh",
+  //       borderRadius: 0,
+  //       top: 0,
+  //       left: "0%",
+  //       xPercent: 0,
+  //       yPercent: 50,
+  //       ease: "expo.out",
+  //       scrollTrigger: {
+  //         trigger: sectionRef.current,
+  //         start: "top top",
+  //         end: "top+=1000vh",
+  //         scrub: true,
+  //         pin: false,
+  //       },
+  //     });
+  //   });
 
-    mm.add("(max-width: 1550px)", () => {
-      // 📌 Мобільна / планшет
-      gsap.set(imgRef.current, {
-        width: "172px",
-        height: "65px",
-        borderRadius: "20px",
-        position: "absolute",
-        top: "230px",
-        left: "500px",
-        xPercent: 0,
-        yPercent: 0,
-      });
+  //   mm.add("(max-width: 1550px)", () => {
+  //     // 📌 Мобільна / планшет
+  //     gsap.set(imgRef.current, {
+  //       width: "172px",
+  //       height: "65px",
+  //       borderRadius: "20px",
+  //       position: "absolute",
+  //       top: "230px",
+  //       left: "500px",
+  //       xPercent: 0,
+  //       yPercent: 0,
+  //     });
 
-      gsap.to(imgRef.current, {
-        width: "100vw",
-        height: "100vh",
-        borderRadius: 0,
-        top: 0,
-        left: "0%",
-        xPercent: 0,
-        yPercent: 30,
-        ease: "expo.out",
-        scrollTrigger: {
-          trigger: sectionRef.current,
-          start: "top top",
-          end: "top+=1000vh",
-          scrub: true,
-          pin: false,
-        },
-      });
-    });
+  //     gsap.to(imgRef.current, {
+  //       width: "100vw",
+  //       height: "100vh",
+  //       borderRadius: 0,
+  //       top: 0,
+  //       left: "0%",
+  //       xPercent: 0,
+  //       yPercent: 30,
+  //       ease: "expo.out",
+  //       scrollTrigger: {
+  //         trigger: sectionRef.current,
+  //         start: "top top",
+  //         end: "top+=1000vh",
+  //         scrub: true,
+  //         pin: false,
+  //       },
+  //     });
+  //   });
 
-    return () => mm.revert(); // очищення при анмаунті
-  }, []);
+  //   return () => mm.revert(); // очищення при анмаунті
+  // }, []);
 
     // useEffect(() => {
     //   if (!sectionRef.current || !imgRef.current) return;
@@ -127,10 +127,7 @@ const Hero = () => {
 
 
   return (
-    <section className="hero" ref={sectionRef}>
-      <div className="hero__image">
-          <img ref={imgRef} src={HERO_IMG} alt="Hero" />
-        </div>
+    <section className="hero" >
       <div className="hero__container container">
         
         <div className="hero__line hero__line-first" />
