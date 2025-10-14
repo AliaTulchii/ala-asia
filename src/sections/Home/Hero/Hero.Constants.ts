6299da534fb9e6639a2b970ede04e732c7b321db
Halina Tulchii <halendeihalyna@gmail.com>
2025-10-14 21:48:32 +0300

    fixed error

--- src/sections/Home/Hero/Hero.Constants.ts
+++ src/sections/Home/Hero/Hero.Constants.ts
@@ -1,2 +1,2 @@
 export const HERO_IMG = "/assets/images/home/hero/hero.jpg";
-export const HERO_IMG_MOB="../../../../public/assets/images/home/hero/hero-mobile.webp"
+export const HERO_IMG_MOB="/assets/images/home/hero/hero-mobile.webp"
