2a8a3280d737ccd3dd012238a0a47b7502258561
Halina Tulchii <halendeihalyna@gmail.com>
2025-10-20 12:10:43 +0300

    added changes

--- src/components/Accordion/AccordionMobile/AccordionMobile.tsx
+++ src/components/Accordion/AccordionMobile/AccordionMobile.tsx
@@ -47,7 +47,6 @@ const AccordionMobile: React.FC = () => {
             <NavLink
               to={item.path}
               className="accordion-mobile__btn"
-              onClick={() => window.scrollTo(0, 0)}
             >
               {item.btn}
             </NavLink>
